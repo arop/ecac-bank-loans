72b9eb1acf63e082ab14f150822d8bc23d488a76
arop <andre.r.o.pires@gmail.com>
2016-11-20 18:42:27 +0000

    filter datasets included, changed strings to integers

--- chumbo2-train.R
+++ chumbo2-train.R
@@ -1,12 +1,6 @@
 library(dplyr)
 
-women <- client %>% filter( ((birth_number / 100) %% 100) > 50)
-men <- client %>% filter( ((birth_number / 100) %% 100) < 50)
-
-women["birth_number"] <- women$birth_number - 5000
-women[,"gender"] <- "f"
-men[,"gender"] <- "m"
-
+#join women and men
 new_client <- rbind(men,women)
 new_client <- new_client[ with(new_client, order(client_id)),]
 new_client[,"year"] <- floor(new_client$birth_number / 10000)
@@ -27,7 +21,7 @@ colnames(account_disp_trans_loans_train)[18] <- "date_loan"
 colnames(account_disp_trans_loans_train)[19] <- "ammount_loan"
 
 account_balances <- account_disp_trans_loans_train %>% group_by(account_id) %>% slice(date_trans < date_loan && which.max(date_trans))
-sd_balance <- aggregate(trans_train[,"balance"], list(account_id=trans_train$account_id), sd)
+sd_balance <- aggregate(trans_train[,"balance"], list(account_id = trans_train$account_id), sd)
 sd_balance <- subset(sd_balance, select = c("account_id","balance"))
 colnames(sd_balance)[2] <- "balance_sd"
 
@@ -40,7 +34,6 @@ account_balances_sd_cards <- subset(account_balances_sd_cards, select = -c(card_
 
 account_balances_sd_cards_clients <- merge(account_balances_sd_cards, new_client)
 account_balances_sd_cards_clients <- subset(account_balances_sd_cards_clients, select = -district_id)
-
 colnames(account_balances_sd_cards)[3] <- "district_id_account"
 
 all_dataset_without_account_district <- merge(account_balances_sd_cards, client_district)  
@@ -49,4 +42,6 @@ all_dataset <- merge(all_dataset_without_account_district, district, by.x = c("d
 #remove useless ids
 final_dataset <- subset(all_dataset, select = -c(district_id_account,client_id,account_id,disp_id,district_id))
 
-write.csv(final_dataset, file = "C:\\Users\\andre\\Documents\\GitHub\\ecac-bank-loans\\datasets\\train_04.csv", row.names = FALSE)
+final_dataset$type_card[is.na(final_dataset$type_card)] <- 0
+
+write.csv(final_dataset, file = "C:\\Users\\andre\\Documents\\GitHub\\ecac-bank-loans\\datasets\\train_05.csv", row.names = FALSE)
